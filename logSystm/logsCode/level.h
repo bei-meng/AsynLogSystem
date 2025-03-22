fe90cdfaa05776b29c0db7a8bd5e0bdc88c018af
xxh <2943110025@qq.com>
2025-03-22 10:10:14 +0800

    完成log的日志等级

--- logSystm/logsCode/level.h
+++ logSystm/logsCode/level.h
@@ -8,5 +8,22 @@ public:
     enum class value{DEBUG,INFO,WARN,ERROR,FATAL};
 
     // 日志等级的字符串转换接口
+    static const char* ToSring(value level){
+        switch(level){
+            case value::DEBUG:
+                return "DEBUG";
+            case value::INFO:
+                return "INFOR";
+            case value::WARN:
+                return "WARN";
+            case value::ERROR:
+                return "ERROR";
+            case value::FATAL:
+                return "FATAL";
+            default:
+                return "UNKNOW";
+        }
+        return "UNKNOW";
+    }
 };
 }
